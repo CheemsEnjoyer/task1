9c9111bf1b84cdf1097dd592e1ef23c2b37f916a
CheemsEnjoyer <95558369+CheemsEnjoyer@users.noreply.github.com>
2024-02-26 14:17:15 +0800

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(97, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сторона а";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(97, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Сторона b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(97, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Сторона с";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(100, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(178, 73);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 22);
+            this.textBoxA.TabIndex = 4;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(178, 118);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 22);
+            this.textBoxB.TabIndex = 5;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(178, 164);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(100, 22);
+            this.textBoxC.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 303);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTextBoxValues();
+        }
+
+        private void LoadTextBoxValues()
+        {
+            textBoxA.Text = Properties.Settings.Default.textBoxA;
+            textBoxB.Text = Properties.Settings.Default.textBoxB;
+            textBoxC.Text = Properties.Settings.Default.textBoxC;
+        }
+
+        private void SaveTextBoxValues()
+        {
+            Properties.Settings.Default.textBoxA = textBoxA.Text;
+            Properties.Settings.Default.textBoxB = textBoxB.Text;
+            Properties.Settings.Default.textBoxC = textBoxC.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBoxA.Text, out double a) &&
+            double.TryParse(textBoxB.Text, out double b) &&
+            double.TryParse(textBoxC.Text, out double c))
+            {
+                string result = Logic.checkExistence(a, b, c);
+                MessageBox.Show(result);
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числа для сторон треугольника.");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveTextBoxValues();
+        }
+    }
+
+
+    public class Logic
+    {
+        public static string checkExistence(double a, double b, double c)
+        {
+            string outMessageExists = "";
+            if ((a < b + c) && (b < a + c) && (c < a + b))
+            {
+                if ((a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == a * a + b * b))
+                {
+                    outMessageExists = "Треугольник является прямоугольным!";
+                }
+                else
+                {
+                    outMessageExists = "Треугольник не является прямоугольным!";
+                }
+            }
+            else
+            {
+                outMessageExists = "Треугольник не существует!";
+            }
+            return outMessageExists;
+        }
+    }
+}
